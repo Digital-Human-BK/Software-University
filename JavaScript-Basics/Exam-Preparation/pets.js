dab94b91fd4a31ddc5d9a00e1e6fe325df23bc08
BiserKaradzhov <linkin@mail.bg>
2021-07-09 23:24:09 +0300

    add files to ExamPreparation

---
+++ JavaScript-Basics/Exam-Preparation/pets.js
@@ -0,0 +1,21 @@
+function pets(input) {
+  let days = Number(input[0])
+  let foodLeft = Number(input[1])
+  let dogFood = Number(input[2])
+  let catFood = Number(input[3])
+  let turtleFood = Number(input[4]) / 1000;
+
+  let foodConsumed = (dogFood * days) + (catFood * days) + (turtleFood * days);
+  
+  if (foodLeft >= foodConsumed) {
+    console.log(`${Math.floor(foodLeft - foodConsumed)} kilos of food left.`);
+  } else {
+    console.log(`${Math.ceil(foodConsumed - foodLeft)} more kilos of food are needed.`);
+  }
+}
+pets(["2",
+  "10",
+  "1",
+  "1",
+  "1200"
+])
