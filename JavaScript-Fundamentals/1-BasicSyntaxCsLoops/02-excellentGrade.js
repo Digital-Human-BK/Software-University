03ac0a0af839f09fe9a93b25779ddb597d65bae3
BiserKaradzhov <linkin@mail.bg>
2021-07-17 12:49:03 +0300

    add lab files to BasicSyntax

---
+++ JavaScript-Fundamentals/1-BasicSyntaxCsLoops/02-excellentGrade.js
@@ -0,0 +1,5 @@
+function solve(input) {
+  input >= 5.5 ? console.log("Excellent") : console.log("Not excellent");
+}
+solve(5.5)
+solve(4.35)
---
+++ JavaScript-Fundamentals/1-BasicSyntaxCsLoops/04-numNto1.js
@@ -0,0 +1,6 @@
+function solve(n){
+  for (let i = n;i >= 1;i--){
+    console.log(i);
+  }
+}
+solve(5)
