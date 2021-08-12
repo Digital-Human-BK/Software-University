91dad1a1d6704c57377bb5b69383670d3b46e240
BiserKaradzhov <linkin@mail.bg>
2021-08-12 23:46:04 +0300

    add exercise files to DataTypesAndVariables

---
+++ JavaScript-Fundamentals/2-DataTypesAndVariables/105-poundsToDollars.js
@@ -0,0 +1,6 @@
+function poundToDollar(gbp) {
+ let dollar = gbp * 1.31;
+ console.log(dollar.toFixed(3));
+}
+poundToDollar(89)
+poundToDollar(39)
---
+++ JavaScript-Fundamentals/2-DataTypesAndVariables/107-lowerToUpper.js
@@ -0,0 +1,11 @@
+function lowUp(char){
+ let toUpper = char.toUpperCase();
+
+ if ( toUpper == char){
+   console.log("upper-case");
+ } else {
+   console.log("lower-case");
+ }
+}
+lowUp("L")
+lowUp("f")
