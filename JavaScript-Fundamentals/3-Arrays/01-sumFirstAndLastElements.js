7b3f4cfd838b22bb1293958f402ba716911b5a84
BiserKaradzhov <linkin@mail.bg>
2021-08-16 22:14:32 +0300

    add lab files to Arrays

---
+++ JavaScript-Fundamentals/3-Arrays/01-sumFirstAndLastElements.js
@@ -0,0 +1,8 @@
+function firstLast(numbers) {
+ let first = numbers[0]
+ let last = numbers[numbers.length - 1];
+ console.log(first+last);
+}
+firstLast([20, 30 ,40])
+firstLast([10, 17, 22, 33])
+firstLast([11, 58, 69])
