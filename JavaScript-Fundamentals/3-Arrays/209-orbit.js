56b5cb18d43b379413e1051edbeb9fdd5e040ed2
BiserKaradzhov <linkin@mail.bg>
2021-09-07 10:40:31 +0300

    add more exercise files to Arrays

---
+++ JavaScript-Fundamentals/3-Arrays/209-orbit.js
@@ -0,0 +1,11 @@
+function orbit([width, height, x, y]) {
+  var a = new Array(5);
+  console.log(a[0]); // returns 1
+  console.log(a.length); // returns 5
+  console.log(a.toString());
+  
+
+}
+orbit([4, 4, 0, 0])
+// orbit([5, 5, 2, 2])
+// orbit([3, 3, 2, 2])
