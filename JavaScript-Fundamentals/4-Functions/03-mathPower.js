1ce55fa9ddd1eef3f493d3b1c1231722cf5637b4
BiserKaradzhov <linkin@mail.bg>
2021-09-08 14:02:59 +0300

    add lab files to Functions

---
+++ JavaScript-Fundamentals/4-Functions/03-mathPower.js
@@ -0,0 +1,14 @@
+function mathPow(n, exponent) {
+
+  /* let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= n
+   } */
+
+  // let result = n ** exponent;
+    
+  let result = Math.pow(n, exponent)
+  console.log(result);
+}
+mathPow(2, 8)
+mathPow(3, 4)
