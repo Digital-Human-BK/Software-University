4f2bf679240cb519800c5c8eec78c8d50dcb6460
BiserKaradzhov <linkin@mail.bg>
2022-01-25 18:00:49 +0200

    add exercise solution files

---
+++ JavaScript-Advanced/2-ArraysAndNestedArrays/101-printArrayWithGivenDelimiter.js
@@ -0,0 +1,18 @@
+function printArray(array, delimiter){
+  return array.join(delimiter)
+}
+printArray(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+)
+printArray(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+)
