67aa7f6eb7d6ae326558257d82000ec5101ffe3c
BiserKaradzhov <linkin@mail.bg>
2022-01-31 22:11:09 +0200

    add lab solution files

---
+++ JavaScript-Advanced/3-ObjectsAndComposition/01-cityRecords.js
@@ -0,0 +1,16 @@
+function cityTaxes(name, population, treasury){
+  let city = {
+    name,
+    population,
+    treasury
+  }
+  return city;
+}
+cityTaxes('Tortuga',
+7000,
+15000
+)
+cityTaxes('Santo Domingo',
+12000,
+23500
+)
