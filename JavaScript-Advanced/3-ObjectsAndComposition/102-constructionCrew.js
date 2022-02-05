29cdc71faf23e5f3b115db6000ab1350f3135220
BiserKaradzhov <linkin@mail.bg>
2022-02-05 18:58:29 +0200

    add exercise solution files

---
+++ JavaScript-Advanced/3-ObjectsAndComposition/102-constructionCrew.js
@@ -0,0 +1,22 @@
+function constructionCrew(worker){
+  if (worker.dizziness == true) {
+    worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
+  }
+
+  return worker;
+}
+constructionCrew({ weight: 80,
+  experience: 1,
+  levelOfHydrated: 0,
+  dizziness: true }
+)
+constructionCrew({ weight: 120,
+  experience: 20,
+  levelOfHydrated: 200,
+  dizziness: true }
+)
+constructionCrew({ weight: 95,
+  experience: 3,
+  levelOfHydrated: 0,
+  dizziness: false }
+)
