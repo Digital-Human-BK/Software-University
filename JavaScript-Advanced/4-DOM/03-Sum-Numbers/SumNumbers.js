21996ba844b7c3c2e959ab1c43850d9faa0b5ecf
BiserKaradzhov <linkin@mail.bg>
2022-02-16 22:51:10 +0200

    add lab solution files

---
+++ JavaScript-Advanced/4-DOM/03-Sum-Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    const a = Number(document.getElementById('num1').value);
+    const b = Number(document.getElementById('num2').value);
+
+    document.getElementById('sum').value = a + b
+}
