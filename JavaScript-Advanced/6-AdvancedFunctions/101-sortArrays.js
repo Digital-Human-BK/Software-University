c1b571fc2dc8a843616041438e4cb3ac0da29fa1
BiserKaradzhov <linkin@mail.bg>
2022-03-12 22:06:32 +0200

    add exercise solution files

---
+++ JavaScript-Advanced/6-AdvancedFunctions/101-sortArrays.js
@@ -0,0 +1,8 @@
+function sortArray(arr, sortOrder) {
+  return sortOrder === 'asc'
+    ? arr.sort((a, b) => a - b)
+    : arr.sort((a, b) => b - a);
+}
+
+sortArray([14, 7, 17, 6, 8], 'asc')
+sortArray([14, 7, 17, 6, 8], 'desc')
