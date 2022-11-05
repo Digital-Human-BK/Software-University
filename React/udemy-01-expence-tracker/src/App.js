1dcbec25e5a94f0b0a03edad6ea7444b7d4d21dd
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2022-11-05 12:44:20 +0200

    TypeScript basics

---
+++ React/udemy-01-expence-tracker/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className='app'>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ TypeScript/src/main.ts
@@ -0,0 +1,9 @@
+let myName: string;
+let myNum: number;
+let isLoading: boolean;
+let type: any;
+
+myName = 'Mike';
+myNum = 35;
+isLoading = true;
+type = true;
